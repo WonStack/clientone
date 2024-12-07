3239d688fc5c3f87ac031d5b6b133758a494404a
owonie <chlehdnjs@gmail.com>
2024-12-08 00:00:49 +0900

    feat: 브라우저에 index 띄우기

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("Error reading index.html");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
