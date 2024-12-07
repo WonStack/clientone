9196b02405ed04fa425b8a45970a0f3511639f41
owonie <chlehdnjs@gmail.com>
2024-12-08 01:59:36 +0900

    feat: add pages

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import http from "http";
-import fs from "fs";
-import path from "path";
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const { IncomingMessage, ServerResponse } = require("http");
 
 const hostname = "localhost";
 const port = 3000;
@@ -8,6 +10,30 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("Error reading index.html");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/about") {
+    const filePath = path.join(__dirname, "about.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("Error reading index.html");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/contact") {
+    const filePath = path.join(__dirname, "contact.html");
     fs.readFile(filePath, "utf-8", (err, data) => {
       if (err) {
         res.statusCode = 500;
